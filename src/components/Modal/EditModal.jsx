2c3fe2ebe518bb1ad275ac62fcca8739f26adf67
ruzan.avetisyan <ruzan.avetisyan@cognaize.com>
2023-12-22 14:39:40 +0400

    test

--- src/components/Modal/EditModal.jsx
+++ src/components/Modal/EditModal.jsx
@@ -7,7 +7,7 @@ const EditModal = (props) => {
   return (
     <div className={showHideClassName}>
       <section className="modal-main">
-        <h1>Modal1</h1>
+        <h1>Modal13</h1>
         <button onClick={handleClose}>Close</button>
       </section>
     </div>
